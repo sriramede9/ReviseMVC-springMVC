e2e288935d4816f930c7598e15e819ba9cf92c84
sriramede9 <only4programming03@gmail.com>
2019-08-29 16:24:05 -0400

    added scriplets,model attribute more to come

--- src/main/java/com/sri/controller/HomeController.java
+++ src/main/java/com/sri/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.sri.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/hi")
-	public String hello() {
+	public String hello(Model model) {
+		
+		model.addAttribute("name", "Sri Ram Ede");
+		
 		return "welcome";
 	}
+	
 }
--- src/main/webapp/WEB-INF/views/welcome.jsp
+++ src/main/webapp/WEB-INF/views/welcome.jsp
@@ -5,10 +5,17 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
+<%@ page isELIgnored="false" %>
 </head>
 <body>
 
-	<h2>Yo Bro !! Long time no show!!</h2>
+	<h2>Yo Bro ${name}!! Long time no see!!</h2>
+	
+	<!-- we have request out and few more gifted obj from scriplets -->
+
+<% String s=(String)request.getAttribute("name");
+out.print(s);
+%>
 
 </body>
 </html>
